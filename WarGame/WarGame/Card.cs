88e12d9fa750a44085b5268a89b4e3864fa75d61
JoshBatchelor777 <joshbatchelor777@gmail.com>
2017-11-20 18:47:16 -0800

    Add files via upload

---
+++ WarGame/WarGame/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarGame
+{
+    // Cards have a suit and kind.
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Kind { get; set; }
+    }
+}
